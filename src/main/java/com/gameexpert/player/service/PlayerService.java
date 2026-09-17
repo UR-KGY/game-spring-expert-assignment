dee8fc16080877ff19b7446ae26e3eaad1a9300c
ur-kgy <0000kgy@gmail.com>
2026-09-17 12:50:20 +0900

    3. 요청 검증과 DTO : 플레이어 등록 요청 DTO의 클래스에 @Validated , 필드에 @NotBlank, @Size , @Pattern 으로 명세서대로 완성 controller 의 POSTMAPPING, 성공했을 경우 201 Service의 DB에서 요청과 동일한 닉네임이 있는지 찾고 있다면 Coflict 예외 , GlobalException 클래스에서 해당 예외를 409로 반환하는 것을 확인 ,   테스트코드 전부 정상 작동, 인게임 닉네임 적용, 동일한 닉네임 허용불가 확인

--- src/main/java/com/gameexpert/player/service/PlayerService.java
+++ src/main/java/com/gameexpert/player/service/PlayerService.java
@@ -2,6 +2,7 @@ package com.gameexpert.player.service;
 
 import com.gameexpert.player.repository.PlayerRepository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +12,7 @@ import com.gameexpert.player.entity.Player;
 
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PlayerService {
@@ -20,7 +22,15 @@ public class PlayerService {
     @Transactional
     public void createPlayer(CreatePlayerRequest request) {
         // TODO Lv 3: 닉네임 중복을 확인하고 플레이어를 저장합니다.
-        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
+        boolean isPlayerEmpty = playerRepository.findByNickname(request.getNickname())
+                .isEmpty();
+
+        if(!isPlayerEmpty){
+            throw new ConflictException("DUPLICATE_NICKNAME");
+        }
+
+        savePlayer(new Player(request.getNickname()));
+//        throw new UnsupportedOperationException("Lv 3: 플레이어 등록을 구현하세요.");
     }
 
     private void savePlayer(Player player) {
